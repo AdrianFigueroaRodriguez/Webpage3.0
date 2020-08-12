c5a4081b965474b8d01c15862972ade2aa5807bf
Adrian Figueroa Rodrigue <adrianfigueroarodriguez@outlook.com>
2020-08-12 10:53:28 -0600

    Dia 4

--- js/index.js
+++ js/index.js
@@ -16,6 +16,7 @@ setTimeout(function(){
     $('#m-is-s').css('opacity', '0.001%');
     $('.n-ml-c').css('opacity', '100%');
     $('.n-o-c').css('opacity', '100%');
+    $('#card-sr').css('height', 'auto');
 }, 2000);
 
 //main screen setup
@@ -48,28 +49,7 @@ function ndc(){
     }
 }
 
-function expand(){
-    if((window.matchMedia("(min-width: 992.01px)").matches)){
-        if(document.getElementById('n-ns-sc1').classList.contains('col-lg-12')){
-            document.getElementById('n-ns-sc1').classList.remove('col-lg-12');
-            document.getElementById('n-ns-sc1').classList.add('col-lg-6');
-            setTimeout(function(){
-                $('#n-ns-sc2').css('display','block');
-            }, 1000);
-        } else{
-            $('#n-ns-sc2').css('display','none');
-            document.getElementById('n-ns-sc1').classList.remove('col-lg-6');
-            document.getElementById('n-ns-sc1').classList.add('col-lg-12');
-        }
-    }
-}
-
-function cardImgExpandXL(x){
-
-}
-
-
-function cardImgExpandSM_LG(x){
+function cardImgExpandXS(x){
     if($(`${x}`).prop('height') === 200){
         $(`${x}`).css('margin-top', '-173px');
         $(`${x}`).css('height', '187%');
@@ -79,11 +59,61 @@ function cardImgExpandSM_LG(x){
     }
 }
 
-function cardImgExpand(x){
-    if((window.matchMedia("(min-width: 992.001px)").matches)){
-        cardImgExpandXL(x);
+function cardImgExpandS(x){
+    if(($(`${x}`).prop('clientHeight') === 200) && ($(`${x}`).prop('id')) === "cri-id"){
+        $(`${x}`).css('height', '373px');
+        $('.cies-r').hide();
     } else{
-        cardImgExpandSM_LG(x);
+        $('.cies-r').show();
+        $(`${x}`).css('height', '200px');
     }
+    
+}
+
+function cardImgExpandM(x){
+
+}
+
+/*
+This code is for expandingthe cards all the way accros only for XL, L and/or M
+    if(document.getElementById('n-ns-sc1').classList.contains('col-lg-12')){
+        document.getElementById('n-ns-sc1').classList.remove('col-lg-12');
+        document.getElementById('n-ns-sc1').classList.add('col-lg-6');
+        setTimeout(function(){
+            $('#n-ns-sc2').css('display','block');
+        }, 1000);
+    } else{
+        $('#n-ns-sc2').css('display','none');
+        document.getElementById('n-ns-sc1').classList.remove('col-lg-6');
+        document.getElementById('n-ns-sc1').classList.add('col-lg-12');
+    }
+*/
+function cardImgExpandL(x){
+
+}
+
+function cardImgExpandXL(){
+
+
+
+
+
+}
+
+function cardImgExpand(x){
+    if((window.matchMedia("(max-width: 576px)").matches)){
+        cardImgExpandXS(x);
+    } else if((window.matchMedia("(min-width: 576px)").matches && (window.matchMedia("(max-width: 768px)").matches))){
+        cardImgExpandS(x);
+    } else if((window.matchMedia("(min-width: 768px)").matches && (window.matchMedia("(max-width: 992px)").matches))){
+        cardImgExpandM(x);
+    } else if((window.matchMedia("(min-width: 992px)").matches && (window.matchMedia("(max-width: 1200px)").matches))){
+        cardImgExpandL(x);
+    } else {
+        cardImgExpandXL(x);
+    }
+
+
+
 }
 
